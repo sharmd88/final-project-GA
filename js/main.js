0c12206e346321d1b0ad3f0a6efc21e31c7d2e72
sharmd88 <sharm.d88@gmail.com>
2017-07-18 11:42:50 -0700

    j query
    adding fades and beginning of parallx on work one

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var oneTextPosition = $('#one-info').offset().top - 600;
+
 $(document).ready(function() {
 
       $('.link').on('click', function() {
@@ -10,17 +12,37 @@ $(document).ready(function() {
 
   });
 
-$('header').on('scroll', function() {
-  $('header').addClass('.home');
-});
+$('.home_img').fadeIn(2000);
+
+
+function workScroll(){
+  var scrolltop = $(window).scrollTop();
+  var scrollamount = (scrolltop / (docheight-windowheight)) * 100;
+  $('#one-info').css('top', "-scrolltop * .2 + 'px'");
+}
+
 
 $(window).on('scroll', function() {
-	var distanceScrolled = $(window).scrollTop();
+  	var distanceScrolled = $(window).scrollTop();
 
-	console.log('The distance scrolled is: ' + distanceScrolled);
-	if (distanceScrolled >= 600) {
-		$('.ad, .design').fadeIn(1000);
-  } else if (distanceScrolled <= 640) {
-    $('.ad, .design').fadeOut(1000);
-  }
+
+  	// console.log('The distance scrolled is: ' + distanceScrolled);
+  	if (distanceScrolled >= 520) {
+  		$('.ad, .design').fadeIn(1000);
+    } else if (distanceScrolled <= 640) {
+      $('.ad, .design').fadeOut(1000);
+    }
+
+    if (distanceScrolled >= 860) {
+      $('#work_h1').fadeIn(1000);
+    }
+
+    // workScroll();
+    // var docheight = $(document).height(); // height of entire document
+    // var windowheight = $(window).height();// height of browser window
+
+    var scrolltop = $(window).scrollTop();
+    // var scrollamount = (scrolltop / (docheight-windowheight)) * 100;
+    // $('#one-info').css('top', scrolltop * .1 + 'px');
+    $('#one-info').css('top', oneTextPosition - (scrolltop * .5));
 });
