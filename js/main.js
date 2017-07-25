27a195ebd4a58b49e03b9376a2c5c95009da5450
sharmd88 <sharm.d88@gmail.com>
2017-07-24 18:23:53 -0700

    css and jquery for mobile
    starting media query for mobile
    removing jquery for mobile view

--- js/main.js
+++ js/main.js
@@ -15,64 +15,72 @@ $(document).ready(function() {
           }, 1000);
       });
 
+    var windowWidth = $( window ).width();
 
-    $('.home_img').fadeIn(2000);
+    if (windowWidth >= 660) {
 
-
-    function workScroll(){
-      var scrolltop = $(window).scrollTop();
-      var scrollamount = (scrolltop / (docheight-windowheight)) * 100;
-      $('#one-info').css('top', "-scrolltop * .2 + 'px'");
+      $('.home_img').fadeIn(2000);
     }
 
-
-    $(window).on('scroll', function() {
-      	var distanceScrolled = $(window).scrollTop();
-
-
-      	console.log('The distance scrolled is: ' + distanceScrolled);
-      	if (distanceScrolled >= 520) {
-      		$('.ad, .design').fadeIn(1000);
-        } else if (distanceScrolled <= 640) {
-          $('.ad, .design').fadeOut(1000);
-        }
-
-        if (distanceScrolled >= 860) {
-          $('#work_h1').fadeIn(1000);
-        } else if (distanceScrolled <= 870) {
-          $('#work_h1').fadeOut(1000);
-        }
-
-        if (distanceScrolled >= 3400) {
-      		$('.hello h1').fadeIn(1000);
-        } else if (distanceScrolled <= 3800) {
-          $('.hello h1').fadeOut(1000);
-        }
-
-        if (distanceScrolled >= 3500) {
-          $('.hello p, .social-links').fadeIn(1000);
-        } else if (distanceScrolled <= 3800) {
-          $('.hello p, .social-links').fadeOut(1000);
-        }
-
-        if ((distanceScrolled >= 1515) && (distanceScrolled <=3700)) {
-          $('h3 a, li a').addClass('nav_load');
-        } else if (distanceScrolled <=1510){
-          $('h3 a, li a').removeClass('nav_load');
-        } else if (distanceScrolled >= 3800){
-          $('h3 a, li a').removeClass('nav_load');
-        }
-
-        // workScroll();
-        // var docheight = $(document).height(); // height of entire document
-        // var windowheight = $(window).height();// height of browser window
-
+      function workScroll(){
         var scrolltop = $(window).scrollTop();
+        var scrollamount = (scrolltop / (docheight-windowheight)) * 100;
+        $('#one-info').css('top', "-scrolltop * .2 + 'px'");
+      }
 
-        $('#one-info').css('top', oneTextPosition - (scrolltop * .5));
-        $('#two-info').css('top', twoTextPosition - (scrolltop * .5));
-        $('#three-info').css('top', threeTextPosition - (scrolltop * .5));
-        // $('img').css('top', oneImgPos - (scrolltop * .2) - 1000)
-      });
+
+      $(window).on('scroll', function() {
+        	var distanceScrolled = $(window).scrollTop();
+
+
+        	console.log('The distance scrolled is: ' + distanceScrolled);
+          var windowWidth = $( window ).width();
+
+          if (windowWidth >= 660) {
+
+          	if (distanceScrolled >= 520) {
+          		$('.ad, .design').fadeIn(1000);
+            } else if (distanceScrolled <= 640) {
+              $('.ad, .design').fadeOut(1000);
+            }
+
+            if (distanceScrolled >= 860) {
+              $('#work_h1').fadeIn(1000);
+            } else if (distanceScrolled <= 870) {
+              $('#work_h1').fadeOut(1000);
+            }
+
+            if (distanceScrolled >= 3400) {
+          		$('.hello h1').fadeIn(1000);
+            } else if (distanceScrolled <= 3800) {
+              $('.hello h1').fadeOut(1000);
+            }
+
+            if (distanceScrolled >= 3500) {
+              $('.hello p, .social-links').fadeIn(1000);
+            } else if (distanceScrolled <= 3800) {
+              $('.hello p, .social-links').fadeOut(1000);
+            }
+
+            if ((distanceScrolled >= 1515) && (distanceScrolled <=3700)) {
+              $('h3 a, li a').addClass('nav_load');
+            } else if (distanceScrolled <=1510){
+              $('h3 a, li a').removeClass('nav_load');
+            } else if (distanceScrolled >= 3800){
+              $('h3 a, li a').removeClass('nav_load');
+            }
+
+            // workScroll();
+            // var docheight = $(document).height(); // height of entire document
+            // var windowheight = $(window).height();// height of browser window
+
+            var scrolltop = $(window).scrollTop();
+
+            $('#one-info').css('top', oneTextPosition - (scrolltop * .5));
+            $('#two-info').css('top', twoTextPosition - (scrolltop * .5));
+            $('#three-info').css('top', threeTextPosition - (scrolltop * .5));
+            // $('img').css('top', oneImgPos - (scrolltop * .2) - 1000)
+          }
+        });
 
 });
